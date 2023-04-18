d8b70f260ec64a36b319f47b58b490b592cf8791
nwolniak <niedzwiedz107@gmail.com>
2023-04-18 21:51:29 +0200

    OSM way deserializer

--- src/main/java/pl/edu/agh/touristsurveys/model/Building.java
+++ src/main/java/pl/edu/agh/touristsurveys/model/Building.java
@@ -2,4 +2,8 @@ package pl.edu.agh.touristsurveys.model;
 
 import java.util.Map;
 
-public record Building(String id, String type, Map<String, String> tags, double lon, double lat) {}
+public record Building(String id,
+                       String type,
+                       Map<String, String> tags,
+                       double lon,
+                       double lat) {}
