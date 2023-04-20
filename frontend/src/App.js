255d806d1e2ba9bf9aaee981b6180a56cc69893c
Piotr Wilkosz <pwilkosz99@gmail.com>
2023-04-21 00:46:01 +0200

    Marker clouster group

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import "leaflet/dist/leaflet.css"
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon } from 'leaflet';
+import { Icon, divIcon, point } from 'leaflet';
+import MarkerClousterGroup from 'react-leaflet-markercluster';
 
 function App() {
 
@@ -27,29 +28,43 @@ function App() {
     iconSize: [38, 38],
   })
 
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: 'marker-cluster-custom',
+      iconSize: point(33, 33, true),
+    })
+}
 
-  return (
-    <div className="App">
-      <h1>Tourist surveys</h1>
 
-      {/* KRK */}
-      <MapContainer center={[50.0614, 19.9365]} zoom={13} >
+return (
+  <div className="App">
+    <h1>Tourist surveys</h1>
 
-        <TileLayer
-          url="https:/{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+    {/* KRK */}
+    <MapContainer center={[50.0614, 19.9365]} zoom={13} >
 
+      <TileLayer
+        url="https:/{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <MarkerClousterGroup
+        chunkedLoading={true}
+        iconCreateFunction={createCustomClusterIcon}
+      >
         {markers.map(marker => (
           <Marker position={marker.geocode} icon={customIcon}>
             <Popup position={marker.geocode}> <h1>{marker.name}</h1></Popup>
           </Marker>
         ))
-
         }
 
-      </MapContainer>
-    </div>
-  );
+      </MarkerClousterGroup>
+
+    </MapContainer>
+
+  </div >
+);
 }
 
 export default App;
