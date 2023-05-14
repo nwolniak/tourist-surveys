f1f1ce7aacfb9d3ff7b6e549cf045bd0bf58dae4
Piotr Wilkosz <pwilkosz99@gmail.com>
2023-05-14 23:15:57 +0200

    CORS set-up

--- src/main/java/pl/edu/agh/touristsurveys/controller/SurveyController.java
+++ src/main/java/pl/edu/agh/touristsurveys/controller/SurveyController.java
@@ -1,7 +1,12 @@
 package pl.edu.agh.touristsurveys.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.touristsurveys.model.SurveyDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class SurveyController {
@@ -9,6 +14,17 @@ public class SurveyController {
     @GetMapping("/test")
     public int test() {
         return 1;
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/getSurveyResults")
+    public List<SurveyDTO> getSurveyResults() {
+        var surveyResults = new ArrayList<SurveyDTO>();
+
+        surveyResults.add(new SurveyDTO("aaa", "Yes"));
+        surveyResults.add(new SurveyDTO("ccc", "No"));
+        surveyResults.add(new SurveyDTO("eee", "Yes"));
+        return surveyResults;
     }
 
 }
